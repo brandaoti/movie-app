63a90f262effefe2111d16ae08a2fda5c3019e01
Brandao Developer <dannnbrandao@gmail.com>
2021-05-06 16:23:08 -0300

    add novas informações recuperadas da api e passando na tela de detalhes

--- lib/app/shared/models/movie.dart
+++ lib/app/shared/models/movie.dart
@@ -5,8 +5,18 @@ class Movie {
   final String title;
   final String posterPath;
   final String releaseDate;
+  final String backdropPath;
+  final String overview;
+  final double popularity;
+  final double voteAverange;
+  final int voteCount;
 
   Movie({
+    this.backdropPath,
+    this.overview,
+    this.popularity,
+    this.voteAverange,
+    this.voteCount,
     @required this.title,
     @required this.posterPath,
     @required this.releaseDate,
@@ -17,6 +27,8 @@ class Movie {
       title: json["title"],
       posterPath: json["poster_path"],
       releaseDate: json["release_date"],
+      backdropPath: json["backdrop_path"],
+      overview: json["overview"],
     );
   }
 }
