51889764566c7863789aeb444df1f36c66b12508
Daniel <dannnbrandao@gmail.com>
2021-06-15 02:41:16 -0300

    atualizado instancia do test, com a nova classe MovieResponse

--- test/movie_model_test.dart
+++ test/movie_model_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movie_app/app/pages/movie/movie_model.dart';
 import 'package:movie_app/app/shared/models/movie.dart';
+import 'package:movie_app/app/shared/models/movie_response.dart';
 import 'package:movie_app/app/shared/repository/api/repository_api.dart';
 
 void main() {
@@ -12,27 +13,22 @@ void main() {
 
     expect(model.movies, completion(isNotNull));
 
-    model.movies.then((movies) {
-      expect(movies[1].title, 'Mortal Kombat');
+    model.movies.then((item) {
+      expect(item.movies[0].title, 'Mortal Kombat');
     });
   });
 }
 
 class MokeAPI extends RepositoryApi {
   @override
-  Future<List<Movie>> fetchMovie() {
-    final movie = [
-      Movie(
-        title: 'Godzilla vs. Kong',
-        posterPath: 'posterPath',
-        releaseDate: 'releaseDate',
-      ),
-      Movie(
-        title: 'Mortal Kombat',
-        posterPath: 'posterPath',
-        releaseDate: 'releaseDate',
-      ),
-    ];
+  Future<MovieResponse> fetchMovie() {
+    final temp = Movie(
+      title: 'Mortal Kombat',
+      posterPath: 'posterPath',
+      releaseDate: 'releaseDate',
+    );
+
+    final movie = MovieResponse(movies: [temp]);
 
     return Future.value(movie);
   }
