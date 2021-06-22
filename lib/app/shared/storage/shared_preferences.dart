791c312a609f2f7ddff1a7dbc3ea7197cc364ab8
Daniel <dannnbrandao@gmail.com>
2021-06-21 21:45:28 -0300

    standby sharedPreferences

--- lib/app/shared/storage/shared_preferences.dart
+++ lib/app/shared/storage/shared_preferences.dart
@@ -7,9 +7,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferencesAdapter extends InternalStorageAdapter {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  List<String> favoriteMovies = [];
-  bool isFavorite = false;
-
   @override
   void toggleFavorite(Movie movie) {
     // if (favoriteMovies.contains(movie.id)) {
@@ -17,21 +14,25 @@ class SharedPreferencesAdapter extends InternalStorageAdapter {
     // } else {
     //   saveFavorite(movie.id);
     // }
-    print('SS');
   }
 
   @override
-  Future<void> saveFavorite(int id, Map<String, dynamic> json) async {
+  Future<String> getFavoriteMovie(int id) async {
     final internal = await _prefs;
-    final response = jsonEncode(json);
-    await internal.setString('$id', response);
-    print('Salvo');
+    return internal.getString(id.toString());
   }
 
   @override
-  Future<void> removeFavorite(int id) async {
+  removeFavorite(int id) async {
     final internal = await _prefs;
-    await internal.remove('$id');
-    print('Removido');
+    internal.remove(id.toString());
+    print('$id Deletado');
+  }
+
+  @override
+  saveFavorite(int id) async {
+    final internal = await _prefs;
+    internal.setString("id", id.toString());
+    print('$id Salvo');
   }
 }
