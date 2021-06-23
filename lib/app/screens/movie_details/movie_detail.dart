db53929b55c5e3a6525153a913e001ea45f6ec1f
Daniel <dannnbrandao@gmail.com>
2021-06-23 05:13:41 -0300

    Adicionado botão pop, voltando para tela home, tirando a instancia do appBar

--- lib/app/screens/movie_details/movie_detail.dart
+++ lib/app/screens/movie_details/movie_detail.dart
@@ -23,11 +23,11 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
     _detailViewModel.loadMovieDetail(_movieID);
 
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        brightness: Brightness.dark,
-      ),
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.transparent,
+      //   brightness: Brightness.dark,
+      // ),
       extendBodyBehindAppBar: true,
       body: Container(
         decoration: BoxDecoration(
@@ -58,30 +58,55 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   // * Container para mostrar imagem
-                  Container(
-                      height: _size.height * .6,
-                      width: _size.width,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(details.posterPath),
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      // * Container para add um gradient/efeito
-                      child: Container(
+                  Stack(
+                    children: [
+                      Container(
+                        height: _size.height * .6,
+                        width: _size.width,
                         decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color(0xFF020024).withOpacity(0.0),
-                              Color(0xFF3f3e70).withOpacity(0.0),
-                              Color(0xFF4E4C61).withOpacity(1),
-                            ],
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            stops: [0.0, 1.0, 1.0],
+                          image: DecorationImage(
+                            image: NetworkImage(details.posterPath),
+                            fit: BoxFit.fill,
                           ),
                         ),
-                      )),
+                        // * Container para add um gradient/efeito
+                        child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xFF020024).withOpacity(0.0),
+                                Color(0xFF3f3e70).withOpacity(0.0),
+                                Color(0xFF4E4C61).withOpacity(1),
+                              ],
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              stops: [0.0, 1.0, 1.0],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10.0, top: 30),
+                          child: FloatingActionButton(
+                            elevation: 0,
+                            backgroundColor: Colors.transparent,
+                            splashColor: Colors.pinkAccent,
+                            child: Icon(
+                              Icons.arrow_back_rounded,
+                              color: Colors.pinkAccent,
+                              size: 38.0,
+                            ),
+                            tooltip: 'Voltar para tela anterior',
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  // Pop home
 
                   // nome, data, descrição
                   Expanded(
@@ -203,6 +228,8 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
         ),
       ),
 
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndDocked,
+
       //! FAB
       floatingActionButton: StreamBuilder<MovieDetail>(
           stream: _detailViewModel.stream,
@@ -211,11 +238,12 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
               return FloatingActionButton(
                 elevation: 0,
                 backgroundColor: Colors.transparent,
+                splashColor: Colors.red[800],
                 child: Icon(
-                  snapshot.data.isFavorite != false ? Icons.favorite : Icons.favorite_border,
-                  size: 36.0,
-                  color: Colors.red[800],
-                ),
+                    snapshot.data.isFavorite != false ? Icons.favorite : Icons.favorite_border,
+                    size: 36.0,
+                    color: Colors.red[800]),
+                tooltip: 'Adicionar aos favoritos',
                 onPressed: () {
                   setState(() {
                     _detailViewModel.toggleFavorite(_movieID);
